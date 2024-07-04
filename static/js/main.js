ff9a1c77f48cc943496126ca972f0d9519093883
Oskar <oskarkaklewski@gmail.com>
2024-07-04 13:20:23 +0200

    Housekeeping

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 // Add current year to the footer
 
-const footerYear = document.querySelector(".footer-year");
+const footerYear = document.querySelector('.footer-year');
 
 function handleCurrentYear() {
 	const year = new Date().getFullYear();
@@ -10,20 +10,20 @@ handleCurrentYear();
 
 // Calculate scores (Centor score, Wells score etc.)
 
-const inputs = [...document.getElementsByClassName("form-check-input")];
-const checkboxes = [...document.querySelectorAll("input[type=checkbox]")];
-const radios = [...document.querySelectorAll("input[type=radio]")];
-const score = document.getElementById("score");
+const inputs = [...document.getElementsByClassName('form-check-input')];
+const checkboxes = [...document.querySelectorAll('input[type=checkbox]')];
+const radios = [...document.querySelectorAll('input[type=radio]')];
+const score = document.getElementById('score');
 
 function countScore() {
 	let points = 0;
-	// Sum all ticked checkboxes by their HTML assigned values
+	// Sum all ticked checkboxes by their HTML values
 	for (let i = 0; i < checkboxes.length; i++) {
 		if (checkboxes[i].checked === true) {
 			points = points + parseInt(checkboxes[i].value);
 		}
 	}
-	// Sum all marked radios by theis HTML assigned values
+	// Sum all marked radios by their HTML values
 	for (let i = 0; i < radios.length; i++) {
 		if (radios[i].checked === true) {
 			points = points + parseInt(radios[i].value);
@@ -34,5 +34,5 @@ function countScore() {
 
 // Run the function whenever one of the values is changed
 inputs.forEach(function (item) {
-	item.addEventListener("change", countScore);
+	item.addEventListener('change', countScore);
 });
